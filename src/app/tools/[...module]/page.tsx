e1a9a7ceaac6e88ec2f096eefa1b3696df027bf0
bipproduction <bip.production.js@gmail.com>
2024-07-24 10:07:57 +0800

    perbaikan cache

--- src/app/tools/[...module]/page.tsx
+++ src/app/tools/[...module]/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@/lib/db/prisma";
 import { Center, Stack } from "@mantine/core";
 import { ModulePage } from "../_ui/ModulePage";
 
+export const dynamic = "force-dynamic"
 export default async function Page({ params }: { params: { module: string[] } }) {
     const id = params.module[0]
     const prompt = await prisma.promptEnginer.findUnique({ where: { id } })
